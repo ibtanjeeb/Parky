d3a4a972e2ea111eb47b7d534ba9fa79f161ceaa
ibTanjeeb <threepointonefour1993@gmail.com>
2022-06-13 01:59:27 +0600

    Added 8. Upsert Post Action

--- Parky/ParkyWeb/Controllers/NationalParksController.cs
+++ Parky/ParkyWeb/Controllers/NationalParksController.cs
@@ -3,6 +3,7 @@ using ParkyWeb.Models;
 using ParkyWeb.Repository.IRepository;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -35,8 +36,56 @@ namespace ParkyWeb.Controllers
             }
             return View(obj);
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(NationalPark obj)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count() > 0)
+                {
+                    byte[] p1 = null;
+                    using (var fs1 = files[0].OpenReadStream())
+                    {
+                        using (var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }
+                    }
+                    obj.Picture = p1;
+                }
+                else
+                {
+                    var objfromdb = await _npRep.GetAsync(SD.NationalParkAPIPath, obj.Id);
+                    obj.Picture = objfromdb.Picture;
+                }
+
+                if(obj.Id==0)
+                {
+                     await _npRep.CreateAsync(SD.NationalParkAPIPath, obj);
+                }
+                else
+                {
+                    await _npRep.UpdateAsync(SD.NationalParkAPIPath+obj.Id, obj);
+
+                }
+                return RedirectToAction(nameof(Index));
+
+
+
+            }
+            else
+            {
+                return View(obj);
+            }
+        }
+
+
         public async Task<IActionResult> GetAllNationalPark()
         {
+
             return Json(new { data = await _npRep.GetAllAsync(SD.NationalParkAPIPath) });
 
         }
